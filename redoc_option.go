628e3bf66b78b3beec19ceff90d865bd2234b32c
leongchien.koh <leongchien.koh@qumonintelligence.com>
2022-06-27 08:41:03 +0800

    add font family

--- redoc_option.go
+++ redoc_option.go
@@ -5,11 +5,16 @@ type RedocOption struct {
 }
 
 type Theme struct {
-	Sidebar Sidebar `json:"sidebar"`
+	Sidebar    Sidebar    `json:"sidebar"`
+	Typography Typography `json:"typography"`
 }
 
 type Sidebar struct {
 	Level1Items Level1Items `json:"level1Items"`
+}
+
+type Typography struct {
+	FontFamily string `json:"fontFamily"`
 }
 
 type Level1Items struct {
